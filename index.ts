e5a77ec7738773f2c4fd56ccc9ece61df0ca5352
inovafani <inoveka@gmail.com>
2025-08-28 15:35:29 +0700

    Generating qr code

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import tableRoutes from './routes/table.js';
+import { table } from 'console';
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use('/table', tableRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
-  console.log('Response sent');
 });
 
 app.listen(port, () => {
--- routes/table.ts
+++ routes/table.ts
@@ -1,3 +1,39 @@
 import { Router } from 'express';
 import QRCode from 'qrcode';
 import prisma from '../lib/prisma.js';
+import { table } from 'console';
+const router = Router();
+
+// Generate QR code for a table
+router.get('/generate/:tableId', async (require, res) => {
+  const { tableId } = require.params;
+
+  // Loaded url inside QR code
+  const baseUrl = process.env.BASE_URL;
+  const qrUrl = `${baseUrl}/menu?table=${tableId}`;
+
+  try {
+    const qrCode = await QRCode.toDataURL(qrUrl);
+    res.json({ tableId, qrUrl, qrCode });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to generate QR code' });
+  }
+});
+
+router.post('/order', async (req, res) => {
+  const { tableId, items, quantity } = req.body;
+  try {
+    const order = await prisma.order.create({
+      data: {
+        items,
+        quantity,
+        tableId: Number(tableId),
+      },
+    });
+    res.json(order);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to create order' });
+  }
+});
+
+export default router;
